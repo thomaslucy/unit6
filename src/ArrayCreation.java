7648018936ade346bfd9d7274db6c2b884eab587
Lucy <thomalu@ssfss.org>
2019-12-12 08:51:54 -0500

    finished ArrayCreation

--- src/ArrayCreation.java
+++ src/ArrayCreation.java
@@ -11,8 +11,44 @@ public class ArrayCreation {
             return array_average;
         }
     }
+    public static String stringArray(String[] friends){
+        String Friends = "";
+        if (friends.length!=5)
+            return " ";
+        else{
+            Friends += friends[0].length() + " ";
+            Friends += friends[1].length() + " ";
+            Friends += friends[2].length() + " ";
+            Friends += friends[3].length() + " ";
+            Friends += friends[4].length();
+        }
+        return Friends;
+    }
+    public static String countryData() {
+        int picker = (int) (Math.random() * 13);
+        String[] countries = {"China", "Egypt", "France", "South Korea", "Germany", "India", "Japan", "Ghana",
+                "Kenya", "Mexico", "United Kingdom", "Burkina Faso", "United States"};
+
+        String[] capitals = {"Beijing", "Cairo", "Paris", "Seoul", "Berlin", "New Delhi", "Tokyo", "Accra", "Nairobi",
+                "Mexico City", "London", "Ouagadougou", "Washington D.C."};
+
+        String[] languages = {"Mandarin", "Arabic", "French", "Korean", "German", "Hindi", "Japanese", "Akuapem Twi",
+                "Swahili", "Spanish", "English", "French", "English"};
+
+        return "The capital of " + countries[picker] + " is " + capitals[picker] + " and the primary language is " +
+                languages[picker];
+    }
+
     public static void main(String[] args){
         double[] numbers = {89, 75, 100};
         System.out.println(getAverage(numbers));
+        String[] names = {"Fred", "Barney", "Betty", "Wilma", "Pebbles"};
+        System.out.println(stringArray(names));
+        String[] names2 = {"Athos", "Porthos", "Aramis"};
+        System.out.println(stringArray(names2));
+        System.out.println(countryData());
+
+
+
     }
 }
