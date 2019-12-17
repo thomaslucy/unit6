e5d394c82c6c86217f53b17087b10573ed1b47d3
Lucy <thomalu@ssfss.org>
2019-12-17 12:35:16 -0500

    finished TraverseArrays

--- src/TraverseArrays.java
+++ src/TraverseArrays.java
@@ -13,8 +13,20 @@ public class TraverseArrays {
             finalarray[i] = i;
         return finalarray;
     }
+    public static boolean sum28(int[] numbers){
+        boolean result = false;
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++){
+            if (numbers [i] == 2)
+                sum += 2;
+        }
+        if (sum == 8)
+            result = true;
+        return result;
+    }
     public static void main(String[] args){
         System.out.println(countEven(new int[] {2, 1, 2, 3, 4}));
         buildArray(10);
+        System.out.println(sum28(new int[]{2, 3, 2, 2, 4, 2}));
     }
 }
